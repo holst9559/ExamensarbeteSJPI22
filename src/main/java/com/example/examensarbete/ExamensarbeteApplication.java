81e89611ceea5590774aa2068ce10ce308fb96a7
Anton Holst <holst9559@hotmail.com>
2024-01-12 09:00:54 +0100

    initial commit

---
+++ src/main/java/com/example/examensarbete/ExamensarbeteApplication.java
@@ -0,0 +1,13 @@
+package com.example.examensarbete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamensarbeteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamensarbeteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/examensarbete/ExamensarbeteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examensarbete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamensarbeteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
