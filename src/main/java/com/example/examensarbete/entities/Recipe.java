d5db19f6b55764ab202e35e27b033c71b3a4f44b
Anton Holst <holst9559@hotmail.com>
2024-01-12 14:53:25 +0100

    Recipe entity created

---
+++ src/main/java/com/example/examensarbete/entities/Recipe.java
@@ -0,0 +1,83 @@
+package com.example.examensarbete.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "RECIPE")
+public class Recipe implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "recipe_id")
+    private Integer recipeId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "dish_id")
+    private Dish dish;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @NotEmpty
+    @Size(max = 255)
+    @Column(name = "description")
+    private String description;
+
+    @NotNull
+    @Column(name = "prep_time")
+    private Integer prepTime;
+
+    @NotNull
+    @Column(name = "cook_time")
+    private Integer cookTime;
+
+    @NotNull
+    @Column(name = "servings")
+    private Integer servings;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy="recipe")
+    private Set<Instruction> instructions = new HashSet();
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "recipe")
+    private Set<RecipeIngredient> recipeIngredients = new HashSet();
+
+    @Column(name = "img_url")
+    private String imgUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "diet_id")
+    private Diet diet;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Recipe recipe = (Recipe) o;
+        return getRecipeId() != null && Objects.equals(getRecipeId(), recipe.getRecipeId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
