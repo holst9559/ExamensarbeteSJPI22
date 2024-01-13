4c3e9ff3441424dfef19eb666d00e31f181af486
Anton Holst <holst9559@hotmail.com>
2024-01-13 13:49:43 +0100

    Added get all recipies

---
+++ src/main/java/com/example/examensarbete/Controller/RecipeController.java
@@ -0,0 +1,26 @@
+package com.example.examensarbete.Controller;
+
+import com.example.examensarbete.entities.Recipe;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/recipe")
+public class RecipeController {
+    private final RecipeService recipeService;
+
+    public RecipeController(RecipeService recipeService){
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping
+    public List<Recipe> getAllRecipies(){
+        return recipeService.getAllRecipies();
+    }
+
+
+
+}
