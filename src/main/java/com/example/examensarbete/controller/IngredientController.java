c4209ce3762b0d7aaece764b6d75967b8cd15631
Anton Holst <holst9559@hotmail.com>
2024-01-13 17:01:14 +0100

    Started to implement USDA API, need to filter JSON somehow

--- src/main/java/com/example/examensarbete/controller/IngredientController.java
+++ src/main/java/com/example/examensarbete/controller/IngredientController.java
@@ -4,10 +4,16 @@ package com.example.examensarbete.controller;
 import com.example.examensarbete.dto.IngredientDto;
 import com.example.examensarbete.entities.Ingredient;
 import com.example.examensarbete.service.IngredientService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonArray;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/ingredients")
@@ -18,7 +24,7 @@ public class IngredientController {
         this.ingredientService = ingredientService;
     }
 
-
+    /*
     @GetMapping
     public List<Ingredient> getAllIngredients(){
         return ingredientService.getAllIngredients();
@@ -43,6 +49,19 @@ public class IngredientController {
     public String deleteIngredient(@PathVariable int id){
         ingredientService.deleteIngredient(id);
         return "Ingredient with id: " + id + " was deleted.";
+    }
+
+     */
+
+    @GetMapping("/new")
+    public String fetchNewIngredient(@RequestParam String ingredient) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String newIngredient = ingredientService.fetchNewIngredient(ingredient);
+        Map<String, Object> map = objectMapper.readValue(newIngredient, new TypeReference<Map<String,Object>>(){});
+        System.out.println("CONTOLLER LAYER");
+        System.out.println(map.values().stream().filter(c -> c.equals(ingredient)));
+
+        return newIngredient;
     }
 
 }
--- src/main/java/com/example/examensarbete/service/IngredientService.java
+++ src/main/java/com/example/examensarbete/service/IngredientService.java
@@ -1,8 +1,28 @@
 package com.example.examensarbete.service;
 
+import com.google.gson.JsonArray;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class IngredientService {
+    @Value("${app.api.key}")
+    private String apiKey;
+    private final WebClient webClient;
 
+    public IngredientService(WebClient.Builder webClientBuilder){
+        this.webClient = webClientBuilder.baseUrl("https://api.nal.usda.gov/fdc/v1")
+                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(2 * 1024 * 1024))
+                .build();
+    }
+
+    public String fetchNewIngredient(String ingredient){
+        return webClient.get()
+                .uri("/foods/search?query=" + ingredient +"&pageSize=1&dataType=Foundation")
+                .header("X-API-KEY", apiKey)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
 }
