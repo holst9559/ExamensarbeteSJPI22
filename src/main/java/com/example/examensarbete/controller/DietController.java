4884b74395cd49e31460d035900e94f52ca4cec8
Anton Holst <holst9559@hotmail.com>
2024-01-28 21:42:00 +0100

    Small change

--- src/main/java/com/example/examensarbete/controller/DietController.java
+++ src/main/java/com/example/examensarbete/controller/DietController.java
@@ -5,10 +5,11 @@ import com.example.examensarbete.service.DietService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("api/v1/diets")
 public class DietController {
     private final DietService dietService;
--- src/main/java/com/example/examensarbete/controller/DishController.java
+++ src/main/java/com/example/examensarbete/controller/DishController.java
@@ -5,10 +5,11 @@ import com.example.examensarbete.service.DishService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("api/v1/dishes")
 public class DishController {
     private final DishService dishService;
